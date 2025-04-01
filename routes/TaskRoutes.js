7600cd74740c3da7359eb35019604fb9ad5de113
Nav Pallav <pallavnav@gmail.com>
2025-04-02 00:38:59 +0530

    Changes with delete api

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -12,10 +12,10 @@ router.get("/isLive", async (req, res) => {
 
 router.get("/tasks", async (req, res) => {
   try {
-    const {status, priority} = req.query;
+    const { status, priority } = req.query;
     let filter = {};
-    if(status) filter.status = status;
-    if(priority) filter.priority = priority;
+    if (status) filter.status = status;
+    if (priority) filter.priority = priority;
     const tasks = await Task.find(filter);
     res.status(200).json(tasks);
   } catch (error) {
@@ -64,6 +64,25 @@ router.delete("/tasks/:id", async (req, res) => {
   }
 });
 
+router.delete('/tasks', async (req, res) => {
+  try {
+    const { ids } = req.body;
+
+    if (!ids || !Array.isArray(ids) || ids.length === 0) {
+      res.status(400).json({ message: 'Invalid request. Provide array of tasks IDs' });
+    }
+
+    const deletedTasks = await Task.deleteMany({ id: { $in: ids } });
+    if (deletedTasks.deletedCount === 0) {
+      res.status(400).json({ message: 'No task found to delete!' });
+    }
+
+    res.status(200).json({ message: 'Tasks deleted successfully!', deletedCount: deletedTasks.deletedCount });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 router.put("/tasks/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -79,5 +98,6 @@ router.put("/tasks/:id", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
 
 module.exports = router;
